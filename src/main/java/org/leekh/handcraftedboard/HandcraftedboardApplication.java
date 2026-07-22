0f40dc94d18bb5d345d3fbebcc292d22084c7def
kannikii <kwnnh0124@dgu.ac.kr>
2026-07-22 21:15:48 +0900

    feat: first commit

---
+++ src/main/java/org/leekh/handcraftedboard/HandcraftedboardApplication.java
@@ -0,0 +1,13 @@
+package org.leekh.handcraftedboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandcraftedboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandcraftedboardApplication.class, args);
+    }
+
+}
